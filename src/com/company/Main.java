e41a06bd80d11a4fb2a790d1bdb87d2f7fe39844
matveySV <ihtmatvienko@gmail.com>
2022-10-11 22:47:49 +0300

    add random function from 1 to 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x;
+        Random random = new Random();
+        x = random.nextInt(1,10);
+        System.out.println("Your lucky number: " +x);
     }
 }
